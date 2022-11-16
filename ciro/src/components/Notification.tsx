b616fb63532477e63190138a567469ad0dbeee58
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2022-11-16 14:36:44 -0800

    Fix which urls to be shown

--- ciro/src/components/Notification.tsx
+++ ciro/src/components/Notification.tsx
@@ -3,12 +3,12 @@ import React from "react";
 function Notification({ notification }) {
   return (
     <div>
-        <div>{notification.companyName}</div>
+        <div><a href={notification.companyUrl}>{notification.companyName}</a></div>
         <div>{notification.companyPhone}</div>
         <div>{notification.companyAddress}</div>
         <div>What's new</div>
         <div>{notification.notificationBody}</div>
-        <div><a href={notification.companyUrl}>Source</a></div>
+        {notification.notificationSource && <div><a href={notification.notificationSource}>Source</a></div>}
     </div>
   );
 }
