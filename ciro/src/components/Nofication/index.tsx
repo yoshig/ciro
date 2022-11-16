7e8c67c0c2e5f0ffab2ed47d1661b4991a3eb085
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2022-11-16 15:16:49 -0800

    more style changes

--- ciro/src/components/Nofication/index.tsx
+++ ciro/src/components/Nofication/index.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import './styles.css';
+import phone from  '../../images/phone.jpeg';
+import dropPin from  '../../images/dropPin.png';
+import greenDot from  '../../images/greenDot.png';
 
 function Notification({ notification }) {
   return (
     <div className="notificationContainer">
       <div className="notificationHeader">
-        <div className="test"><a href={notification.companyUrl}>{notification.companyName}</a></div>
+        <div className="test">
+          <img height={10} src={greenDot} alt="greenDot"/>
+          <a href={notification.companyUrl}>{notification.companyName}</a>
+        </div>
       </div>
       <div className="notificationBody">
         <div className="notificationContactContainer">
-          <div>{notification.companyPhone}</div>
+          <div><img height={10} src={phone} alt="phone"/>{notification.companyPhone}</div>
+          <div><img height={10} src={dropPin} alt="dropPin"/>{notification.companyPhone}</div>
           <div>{notification.companyAddress}</div>
         </div>
         <div className="boldText">What's new</div>
         <div>{notification.notificationBody}</div>
-        {notification.notificationSource && <div><a href={notification.notificationSource}>Source</a></div>}
+        {notification.notificationSource && <div className="notificationSource"><a href={notification.notificationSource}>Source</a></div>}
       </div>
     </div>
   );
